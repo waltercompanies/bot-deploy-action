6b0cd556a879e2d3566555c1530f08a12ab9cbc0
Diederick Lawson <webmaster@altovista.nl>
2020-06-26 15:16:28 +0200

    Also submit commit author, last commit message and actor

--- dist/index.js
+++ dist/index.js
@@ -13963,12 +13963,32 @@ const request = __webpack_require__(570);
 
 (async () => {
   const event = core.getInput("event");
-  const secret = core.getInput("secret");
-  const url = core.getInput("url");
+  const secret = process.env.WEBHOOK_SECRET ? process.env.WEBHOOK_SECRET : core.getInput("secret");
+  const url = process.env.WEBHOOK_URL ? process.env.WEBHOOK_URL : core.getInput("url");
   const sha = process.env.GITHUB_SHA;
   const repo = process.env.GITHUB_REPOSITORY;
   const ref = process.env.GITHUB_REF;
-  const messageReference = github.context.payload.client_payload.message_reference;
+  const clientPayload = github.context.payload.client_payload;
+  const messageReference = clientPayloud ? clientPayload.message_reference : "";
+  const commits = github.context.payload.commits;
+  let commitMessage = "", commitAuthor = "";
+  let actor = "";
+
+  if (clientPayload && clientPayload.commit_message) {
+    commitMessage = clientPayload.commit_message;
+  } else if (commits.length > 0) {
+    commitMessage = commits[0].message;
+  }
+
+  if (clientPayload && clientPayload.commit_author) {
+    commitAuthor = clientPayload.commit_author;
+  } else if (commits.length > 0) {
+    commitAuthor = commits[0].author.name;
+  }
+
+  if(clientPayload && clientPayload.actor) {
+    actor = clientPayload.actor;
+  }
 
   try {
     const data = {
@@ -13977,7 +13997,10 @@ const request = __webpack_require__(570);
       sha: sha,
       repo: repo,
       ref: ref,
-      message_reference: messageReference
+      message_reference: messageReference,
+      commit_message: commitMessage,
+      commit_author: commitAuthor,
+      actor: actor
     }
 
     await request({ url: url, method: "POST", json: true, body: data });
